055d1ed218e0ecf442d200bb8b557a101e3c253c
XXBAYXX69 <92044631+XXBAYXX69@users.noreply.github.com>
2022-09-23 15:27:47 +0000

    Update index.js

--- static/index.js
+++ static/index.js
@@ -19,3 +19,38 @@ function isUrl(val = ''){
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
     return false;
 };
+(function () { 
+    const rgx = new RegExp(/(WestWestCoast-Chill.up.railway.app
+
+)|(:portNumUsedInLocalDev)|/g); 
+    const host = window.location.host; 
+    const isMatch = !host.match(rgx); 
+ 
+    function showWarning() { 
+        let warning = document.createElement('h1'); 
+        warning.innerText = 'DON\'T STEAL'; 
+        warning.style.fontSize = '5em'; 
+        warning.style.fontWeight = 700; 
+        warning.style.position = 'fixed'; 
+        warning.style.left = `${(window.innerWidth/2)}px`; 
+        warning.style.top =`${ window.innerHeight /2}px`; 
+        warning.style.zIndex = 9999; 
+        document.body.appendChild(warning); 
+    } 
+ 
+    function exactRvg () { 
+        cornify_add(); 
+ 
+            window.setTimeout(()=> { 
+                exactRvg(); 
+            }, 500); 
+ 
+    } 
+    if (isMatch ) { 
+        showWarning(); 
+        $.getScript('https://www.cornify.com/js/cornify.js',function(){ 
+            exactRvg(); 
+        }); 
+    } 
+ 
+})(); 
